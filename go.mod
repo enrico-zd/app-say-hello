220e3bfb05c7a581dd7c483f4d391b2db7faadc0
Enrico Zada <enricozada@icloud.com>
2023-12-16 01:25:01 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/enrico-zd/app-say-hello
+
+go 1.21.2
+
+require github.com/enrico-zd/go-say-hello-tutor v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello_tutor "github.com/enrico-zd/go-say-hello-tutor"
+)
+
+func main() {
+	fmt.Println(go_say_hello_tutor.SayHello())
+}
